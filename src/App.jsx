a1acb858a942185dc4b16a5302124a4e5d27e49e
nov4n-4r <novanadristiawan2264@gmail.com>
2023-07-09 15:14:17 +0700

    Add navigation in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import DrumsApp from "./projects/drums";
 import Pomodoro from "./projects/pomodoro_clock";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router, Link } from "react-router-dom";
+
+function Home(){
+  return <section
+    className="bg-stone-800 fixed flex items-center p-6 justify-center flex-col gap-4"
+    style={{
+        width : "800px",
+        top : "50%",
+        left : "50%",
+        minHeight : "400px",
+        transform : "translate(-50%, -50%)"
+    }}
+  >
+    <Link to="/drums"><button className="p-2 text-white">DRUMS CHALLENGE</button></Link>
+    <Link to="/pomodoro"><button className="p-2 text-white">25 + 5 CLOCK CHALLENGE</button></Link>
+  </section>
+}
 
 function App(){
   return <Router>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/drums" element={<DrumsApp />} />
       <Route path="/pomodoro" element={<Pomodoro />} />
     </Routes>
